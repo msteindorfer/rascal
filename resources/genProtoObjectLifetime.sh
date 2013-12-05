ce9dc89915d29e83ad2502198bd4d86db8e32471
Michael Steindorfer <Michael.Steindorfer@cwi.nl>
2013-12-05 17:42:20 +0100

    Adding protobuf definitions.

---
+++ resources/genProtoObjectLifetime.sh
@@ -0,0 +1,2 @@
+protoc --java_out=../../tracr/src/main/java objectLifetime.proto
+protoc --java_out=../src objectLifetime.proto
---
+++ resources/genProtoPDB.sh
@@ -0,0 +1,2 @@
+protoc --java_out=../../tracr/src/main/java pdb.proto
+protoc --java_out=../src pdb.proto
